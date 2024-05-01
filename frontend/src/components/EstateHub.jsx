42d254b19781ca36cde4637f7859b399ebc8d9fc
Archit0204 <amittal0204@gmail.com>
2024-05-01 17:23:32 +0530

    Logo

---
+++ frontend/src/components/EstateHub.jsx
@@ -0,0 +1,8 @@
+
+export function EstateHub() {
+
+    return (
+        <div className="text-white text-3xl font-semibold">Estate<span className="bg-myViolet-100 ml-1 text-myGrey-150 px-1 rounded-xl">Hub</span></div>
+    )
+
+}
