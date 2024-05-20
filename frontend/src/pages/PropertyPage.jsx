5458f46ad08dae9d226a2373c924fc11d588fec4
Archit0204 <amittal0204@gmail.com>
2024-05-20 20:29:22 +0530

    structuring

--- frontend/src/pages/PropertyPage.jsx
+++ frontend/src/pages/PropertyPage.jsx
@@ -11,43 +11,6 @@ import axios from "axios";
 export function PropertyPage() {
 
     const { id } = useParams();
-    // const property = useRecoilValue(singlePropSelector(id));
-    // console.log(property);
-
-    // const [loading, setLoading] = useState(false);
-    // const [property, setProperty] = useState(null);
-
-    // useEffect(() => {
-    //     console.log("Starting UE");
-    //     async function fetchData() {
-    //         setLoading(true);
-
-    //         const token = localStorage.getItem("token");
-    //         try {
-    //             const response = await axios.get(`http://localhost:3000/api/v1/listings/getProp/${id}`, {
-    //                 headers: {
-    //                     'Content-Type': 'application/json', // Example header
-    //                     'Authorization': `${token}`, // Example header
-    //                 }
-    //             });
-    //             console.log(response);
-    //             if (response.data.success) {
-    //                 setProperty(response.data.property)
-    //             }
-    //             else {
-    //                 toast.error("Error Occurred");
-    //             }
-    //         }
-    //         catch (e) {
-    //             toast.error("Unexprected Error Occurred");
-    //         }
-    //         setLoading(false);
-    //         console.log(property);
-    //     }
-
-    //     fetchData();
-
-    // }, [])
 
     const [loading, setLoading] = useState(false);
     const [property, setProperty] = useState({});
